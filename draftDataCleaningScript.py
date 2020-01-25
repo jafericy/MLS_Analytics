dab8981db11d64c7fc1f6860d326fba152d85564
Jacob Fericy <jafericy1@gmail.com>
2020-01-24 21:42:23 -0500

    Adding new data and prelim files

---
+++ draftDataCleaningScript.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 21:37:11 2020
+
+@author: jacobfericy
+"""
+
+import pandas as pd
+
+
+mlsDraft2019_Raw = pd.read_excel('/Users/jacobfericy/GitHub/MLS_Analytics/MLS_2019_Raw.xlsx')
