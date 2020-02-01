21e67beecb447adb44403862ba69d0c1ce641c1d
Jacob Fericy <jafericy1@gmail.com>
2020-02-01 16:21:38 -0500

    updating python file

--- MLSStandings.py
+++ MLSStandings.py
@@ -9,3 +9,45 @@ Created on Sat Feb  1 13:00:29 2020
 import pandas as pd
 import numpy as np
 import os
+
+
+
+mlsStandingsdict = pd.read_excel('/Users/jacobfericy/GitHub/MLS_Analytics/MLS_Standings.xlsx', sheet_name=None)
+
+mlsStandings = pd.DataFrame()
+for name, sheet in mlsStandingsdict.items():
+    sheet['Sheet'] = name
+    sheet = sheet.rename(columns=lambda x: x.split('\n')[-1])
+    mlsStandings = mlsStandings.append(sheet)
+
+mlsStandings.reset_index(inplace=True, drop=True)
+
+mlsStandings = mlsStandings.drop(['WIR','LIR','Sheet'],axis=1)
+
+
+
+old_name_list = ['Kansas City Wiz','Kansas City Wizards','Columbus Crew', 'MetroStars','Dallas Burn','Los Angeles Galaxy','NY/NJ MetroStars']
+new_name_list = ['Sporting Kansas City','Sporting Kansas City','Columbus Crew SC', 'New York Red Bulls','FC Dallas','LA Galaxy','New York Red Bulls']
+
+
+name_dict = dict(zip(old_name_list, new_name_list))
+
+mlsStandings.replace(name_dict, inplace=True)
+
+
+
+mlsStandings = mlsStandings.replace('Kansas City Wiz', 'Sporting Kansas City')
+
+
+
+
+
+new_team_list = pd.DataFrame(mlsStandings.TEAM.unique())
+  
+  
+len(mlsStandings.TEAM.unique())
+
+
+#print(mlsStandings)
+
+mlsStandings.to_csv (r'/Users/jacobfericy/GitHub/MLS_Analytics/fullMLSStandings.csv', index = None, header=True)
