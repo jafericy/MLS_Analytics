a212e8cc88ce591e877d1bb6d331da60a76561a2
Jacob A. Fericy <jafericy1@gmail.com>
2020-05-24 19:01:15 -0400

    Update MLSStandings.py

--- MLSStandings.py
+++ MLSStandings.py
@@ -10,8 +10,6 @@ import pandas as pd
 import numpy as np
 import os
 
-
-
 mlsStandingsdict = pd.read_excel('/Users/jacobfericy/GitHub/MLS_Analytics/MLS_Standings.xlsx', sheet_name=None)
 
 mlsStandings = pd.DataFrame()
@@ -24,23 +22,12 @@ mlsStandings.reset_index(inplace=True, drop=True)
 
 mlsStandings = mlsStandings.drop(['WIR','LIR','Sheet'],axis=1)
 
-
-
 old_name_list = ['Kansas City Wiz','Kansas City Wizards','Columbus Crew', 'MetroStars','Dallas Burn','Los Angeles Galaxy','NY/NJ MetroStars']
 new_name_list = ['Sporting Kansas City','Sporting Kansas City','Columbus Crew SC', 'New York Red Bulls','FC Dallas','LA Galaxy','New York Red Bulls']
-
 
 name_dict = dict(zip(old_name_list, new_name_list))
 
 mlsStandings.replace(name_dict, inplace=True)
-
-
-
-
-
-
-
-
 
 minlist = pd.DataFrame()
 for i in range(1998,2019):
@@ -52,14 +39,6 @@ for i in range(1998,2019):
 team_count = len(mlsStandings.TEAM.unique())
 
 team_df = pd.DataFrame(mlsStandings.TEAM.unique())
-
-
-
-
-
-
-
-
 
 mlsStandings.to_csv (r'/Users/jacobfericy/GitHub/MLS_Analytics/fullMLSStandings.csv', index = None, header=True)
 
